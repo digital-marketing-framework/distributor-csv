a2f4d59f3f1521bc27a85df6f0ad69d95ad4c258
Meelis Karulin <karulin@mediatis.de>
2023-08-31 11:40:27 +0200

    wip test

--- src/Route/CsvRoute.php
+++ src/Route/CsvRoute.php
@@ -2,12 +2,23 @@
 
 namespace DigitalMarketingFramework\Distributor\Csv\Route;
 
+use DigitalMarketingFramework\Core\ConfigurationDocument\SchemaDocument\Schema\ContainerSchema;
+use DigitalMarketingFramework\Core\ConfigurationDocument\SchemaDocument\Schema\StringSchema;
 use DigitalMarketingFramework\Distributor\Core\DataDispatcher\DataDispatcherInterface;
 use DigitalMarketingFramework\Distributor\Core\Route\Route;
 use DigitalMarketingFramework\Distributor\Csv\DataDispatcher\CsvDataDispatcherInterface;
+use DigitalMarketingFramework\Core\ConfigurationDocument\SchemaDocument\Schema\SchemaInterface;
+
 
 class CsvRoute extends Route
 {
+    /*
+     * example configurations
+     *
+     * fileIdentifier:form-submits.csv
+     * delimiter:;
+     * enclosure:"
+     */
     protected const KEY_FILE_IDENTIFIER = 'fileIdentifier';
     protected const DEFAULT_FILE_IDENTIFIER = 'form-submits.csv';
     protected const KEY_VALUE_DELIMITER = 'delimiter';
@@ -21,29 +32,24 @@ class CsvRoute extends Route
         return 'csv';
     }
 
-    public static function getDefaultConfiguration(): array
+    protected function getDispatcher(): DataDispatcherInterface
     {
-        $config = [
-                static::KEY_FILE_IDENTIFIER => static::DEFAULT_FILE_IDENTIFIER,
-                static::KEY_VALUE_DELIMITER => static::DEFAULT_VALUE_DELIMITER,
-                static::KEY_VALUE_ENCLOSURE => static::DEFAULT_VALUE_ENCLOSURE,
-            ]
-            + parent::getDefaultConfiguration();
-        return $config;
+        /** @var CsvDataDispatcherInterface */
+        $dispatcher = $this->registry->getDataDispatcher($this->getDispatcherKeyword());
+        $dispatcher->setFileIdentifier($this->getConfig(static::KEY_FILE_IDENTIFIER));
+        $dispatcher->setDelimiter($this->getConfig(static::KEY_VALUE_DELIMITER));
+        $dispatcher->setEnclosure($this->getConfig(static::KEY_VALUE_ENCLOSURE));
+        return $dispatcher;
     }
 
-    protected function getDispatcher(): ?DataDispatcherInterface
+    public static function getSchema(): SchemaInterface
     {
-        try {
-            /** @var CsvDataDispatcherInterface */
-            $dispatcher = $this->registry->getDataDispatcher($this->getDispatcherKeyword());
-            $dispatcher->setFileIdentifier($this->getConfig(static::KEY_FILE_IDENTIFIER));
-            $dispatcher->setDelimiter($this->getConfig(static::KEY_VALUE_DELIMITER));
-            $dispatcher->setEnclosure($this->getConfig(static::KEY_VALUE_ENCLOSURE));
-            return $dispatcher;
-        } catch (FileDoesNotExistException $e) {
-            $this->logger->error($e->getMessage());
-            return null;
-        }
+        /** @var ContainerSchema $schema */
+        $schema = parent::getSchema();
+        $schema->addProperty(static::KEY_FILE_IDENTIFIER, new StringSchema(static::DEFAULT_FILE_IDENTIFIER));
+        $schema->addProperty(static::KEY_VALUE_DELIMITER, new StringSchema(static::DEFAULT_VALUE_DELIMITER));
+        $schema->addProperty(static::KEY_VALUE_ENCLOSURE, new StringSchema(static::DEFAULT_VALUE_ENCLOSURE));
+
+        return $schema;
     }
 }
