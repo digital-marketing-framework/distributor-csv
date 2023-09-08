d7300ff2ec65553674dd90f70a61f9fc8ff68cf2
Meelis Karulin <karulin@mediatis.de>
2023-09-08 20:33:38 +0200

    wip

--- src/DistributorCsvInitialization.php
+++ src/DistributorCsvInitialization.php
@@ -3,6 +3,7 @@
 namespace DigitalMarketingFramework\Distributor\Csv;
 
 use DigitalMarketingFramework\Core\Initialization;
+use DigitalMarketingFramework\Core\Registry\RegistryDomain;
 use DigitalMarketingFramework\Distributor\Core\DataDispatcher\DataDispatcherInterface;
 use DigitalMarketingFramework\Distributor\Core\Route\RouteInterface;
 use DigitalMarketingFramework\Distributor\Csv\DataDispatcher\CsvDataDispatcher;
@@ -11,11 +12,14 @@ use DigitalMarketingFramework\Distributor\Csv\Route\CsvRoute;
 class DistributorCsvInitialization extends Initialization
 {
     protected const PLUGINS = [
-        DataDispatcherInterface::class => [
+        RegistryDomain::DISTRIBUTOR => [
+
+            DataDispatcherInterface::class => [
             CsvDataDispatcher::class,
-        ],
-        RouteInterface::class => [
-            CsvRoute::class,
+            ],
+            RouteInterface::class => [
+                CsvRoute::class,
+            ],
         ],
     ];
 
