128febe61302b4efee1e2c39694b289b1e13b3e0
Stephan Ude <ude@mediatis.de>
2023-10-13 18:45:34 +0200

    [TASK] introduced package aliases

--- src/DistributorCsvInitialization.php
+++ src/DistributorCsvInitialization.php
@@ -24,8 +24,8 @@ class DistributorCsvInitialization extends Initialization
 
     protected const SCHEMA_MIGRATIONS = [];
 
-    public function __construct()
+    public function __construct(string $packageAlias = '')
     {
-        parent::__construct('distributor-csv', '1.0.0');
+        parent::__construct('distributor-csv', '1.0.0', $packageAlias);
     }
 }
